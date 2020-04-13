346a3c86862619e7e7c4ceeb9c8107ca9a783780
Jordan Muniz <munizj21@trinityprep.org>
2020-04-13 15:27:53 -0400

    test: Add Tests for SAP class

--- src/test/java/SAPTest.java
+++ src/test/java/SAPTest.java
@@ -9,19 +9,22 @@ import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 
 public class SAPTest {
-
+  private Digraph graph;
   private SAP sapDigraph1;
   @Before
   public void setup() {
     In in = new In("wordnet-test-files/digraph1.txt");
     Digraph G = new Digraph(in);
+    graph = G;
     System.out.println(G);
     sapDigraph1 = new SAP(G);
   }
 
   @Test
   public void testSAPImmutable() {
-    fail("Not yet implemented");
+    int anc = sapDigraph1.ancestor(7,9);
+    graph.reverse();
+    assertEquals(anc,sapDigraph1.ancestor(7,9));
   }
 
   @Test
@@ -29,16 +32,35 @@ public class SAPTest {
     assertEquals(4, sapDigraph1.length(3, 11));
   }
 
-  //TODO: Write a testLengthIntInt test case for 
-  //      digraph-ambiguous-ancestor.txt and another digraph of your choice
+  @Test
+  public void testLengthIntInt2() {
+    In in = new In("wordnet-test-files/digraph-ambiguous-ancestor.txt");
+    Digraph G = new Digraph(in);
+    sapDigraph1 = new SAP(G);
+    assertEquals(3, sapDigraph1.length(5, 1));
+     in = new In("wordnet-test-files/digraph2.txt");
+     G = new Digraph(in);
+    sapDigraph1 = new SAP(G);
+    assertEquals(2, sapDigraph1.length(3, 1));
+
+  }
 
   @Test
   public void testAncestorIntInt() {
     assertEquals(1, sapDigraph1.ancestor(3, 11));
   }
+  @Test
+  public void testAncestorIntInt2() {
+    In in = new In("wordnet-test-files/digraph-ambiguous-ancestor.txt");
+    Digraph G = new Digraph(in);
+    sapDigraph1 = new SAP(G);
+    assertEquals(2, sapDigraph1.ancestor(5, 1));
+    in = new In("wordnet-test-files/digraph2.txt");
+    G = new Digraph(in);
+    sapDigraph1 = new SAP(G);
+    assertEquals(3, sapDigraph1.ancestor(3, 1));
+  }
 
-  //TODO: Write a testAncestorIntInt test case for 
-  //      digraph-ambiguous-ancestor.txt and another digraph of your choice
 
   @Test
   public void testLengthIterableIterable() {
@@ -52,8 +74,36 @@ public class SAPTest {
     assertEquals(3,sapDigraph1.length(verticesV, verticesW));
   }
 
-  //TODO: Write a testLengthIterableIterable test case for 
-  //      digraph-ambiguous-ancestor.txt and another digraph of your choice
+  @Test
+  public void testLengthIterableIterable2() {
+    ArrayList<Integer> verticesV = new ArrayList<Integer>();
+    verticesV.add(1);
+    verticesV.add(3);
+    verticesV.add(5);
+    ArrayList<Integer> verticesW = new ArrayList<Integer>();
+    verticesW.add(10);
+    verticesW.add(4);
+
+    In in = new In("wordnet-test-files/digraph-ambiguous-ancestor.txt");
+    Digraph G = new Digraph(in);
+    sapDigraph1 = new SAP(G);
+    assertEquals(1, sapDigraph1.length(verticesV, verticesW));
+
+     verticesV = new ArrayList<Integer>();
+    verticesV.add(1);
+    verticesV.add(4);
+    verticesW = new ArrayList<Integer>();
+    verticesW.add(2);
+    verticesW.add(5);
+
+
+    in = new In("wordnet-test-files/digraph2.txt");
+    G = new Digraph(in);
+    sapDigraph1 = new SAP(G);
+    assertEquals(1,sapDigraph1.length(verticesV, verticesW));
+  }
+
+
 
   @Test
   public void testAncestorIterableIterable() {
@@ -67,7 +117,35 @@ public class SAPTest {
     assertEquals(5, sapDigraph1.ancestor(verticesV, verticesW));
   }
 
-  //TODO: Write a testAncestorIterableIterable test case for 
-  //      digraph-ambiguous-ancestor.txt and another digraph of your choice
+  @Test
+  public void testAncestorIterableIterable2() {
+    ArrayList<Integer> verticesV = new ArrayList<Integer>();
+    verticesV.add(1);
+    verticesV.add(3);
+    verticesV.add(5);
+    ArrayList<Integer> verticesW = new ArrayList<Integer>();
+    verticesW.add(10);
+    verticesW.add(4);
+
+    In in = new In("wordnet-test-files/digraph-ambiguous-ancestor.txt");
+    Digraph G = new Digraph(in);
+    sapDigraph1 = new SAP(G);
+    assertEquals(4, sapDigraph1.ancestor(verticesV, verticesW));
+
+    verticesV = new ArrayList<Integer>();
+    verticesV.add(1);
+    verticesV.add(4);
+    verticesW = new ArrayList<Integer>();
+    verticesW.add(2);
+    verticesW.add(5);
+
+
+    in = new In("wordnet-test-files/digraph2.txt");
+    G = new Digraph(in);
+    sapDigraph1 = new SAP(G);
+    assertEquals(2,sapDigraph1.ancestor(verticesV, verticesW));
+  }
+
+  
 
 }
