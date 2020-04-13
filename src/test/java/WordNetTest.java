6e7a1059c31cdccfadce6018d847208e74f49eb2
Jordan Muniz <munizj21@trinityprep.org>
2020-04-13 16:19:16 -0400

    test: Add extensive tests for the various classes fix: Fix Wordnet null detection

--- src/test/java/WordNetTest.java
+++ src/test/java/WordNetTest.java
@@ -11,22 +11,28 @@ public class WordNetTest {
   private WordNet wordNet6TwoAncestors;
   @Before
   public void setUp() throws Exception {
-    wordNet6TwoAncestors = new WordNet("synsets6.txt", "hypernyms6TwoAncestors.txt");
+    wordNet6TwoAncestors = new WordNet("wordnet-test-files/synsets6.txt", "wordnet-test-files/hypernyms6TwoAncestors.txt");
   }
 
   @Test (timeout = 5000, expected = IllegalArgumentException.class)
   public void testWordNetNullSysnets() {
-    WordNet w = new WordNet(null, "hypernyms.txt");
+    WordNet w = new WordNet(null, "wordnet-test-files/hypernyms.txt");
   }
   
   @Test (timeout = 5000, expected = IllegalArgumentException.class)
   public void testWordNetNullHypernyms() {
-    WordNet w = new WordNet("sysnets.txt", null);
+    WordNet w = new WordNet("wordnet-test-files/sysnets.txt", null);
   }
-  
-  //TODO Create test case that throws IllegalArgumentException if there is a cycle
-  
-  //TODO Create test case that throws IllegalArgumentException if there are two roots
+
+  @Test (timeout = 5000, expected = IllegalArgumentException.class)
+  public void testWordNetCycleHyper() {
+    WordNet w = new WordNet("wordnet-test-files/sysnets.txt", "wordnet-test-files/hypernyms3InvalidCycle.txt");
+  }
+
+  @Test (timeout = 5000, expected = IllegalArgumentException.class)
+  public void testWordNet2RootsHyper() {
+    WordNet w = new WordNet("wordnet-test-files/sysnets.txt", "wordnet-test-files/hypernyms3InvalidTwoRoots.txt");
+  }
 
   @Test
   public void testNouns() {
@@ -36,40 +42,120 @@ public class WordNetTest {
       assertTrue(expectedNouns.contains(actualNoun));
     }
   }
-  
-  //TODO write testNouns for synsets3 and synsets15
+
+  @Test
+  public void testNouns2() { // synset3 does not have a corresponding hypernym file
+    wordNet6TwoAncestors = new WordNet("wordnet-test-files/synsets8.txt", "wordnet-test-files/hypernyms8ManyAncestors.txt");
+    String[] expectedNounsArray = {"a", "b", "c", "d", "e", "f","g","h"};
+    ArrayList<String> expectedNouns = new ArrayList<>(Arrays.asList(expectedNounsArray));
+    for(String actualNoun: wordNet6TwoAncestors.nouns()) {
+      assertTrue(expectedNouns.contains(actualNoun));
+    }
+  }
+
+  @Test
+  public void testNouns3() {
+    wordNet6TwoAncestors = new WordNet("wordnet-test-files/synsets15.txt", "wordnet-test-files/hypernyms15Tree.txt");
+    String[] expectedNounsArray = {"a", "b", "c", "d", "e", "f","g","h","i","j","k","l","m","n","o"};
+    ArrayList<String> expectedNouns = new ArrayList<>(Arrays.asList(expectedNounsArray));
+    for(String actualNoun: wordNet6TwoAncestors.nouns()) {
+      assertTrue(expectedNouns.contains(actualNoun));
+    }
+  }
 
   @Test
   public void testIsNoun() {
     assertTrue(wordNet6TwoAncestors.isNoun("a"));
   }
-  
-  //TODO write 3 testCases for isNoun for synsets.txt
-  //Tip: pick a word at the beginning, in the middle and a word towards the end
+
+  @Test
+  public void testIsNoun2() {
+    assertTrue(wordNet6TwoAncestors.isNoun("d"));
+  }
+
+  @Test
+  public void testIsNoun3() {
+    assertTrue(wordNet6TwoAncestors.isNoun("f"));
+  }
   
   
   @Test
   public void testIsNounFalse() {
     assertFalse(wordNet6TwoAncestors.isNoun("g"));
   }
-  
-  //TODO write 3 testCases for isNoun for synsets.txt
-  //Tip: pick a word that's close to the dictionary, jumbled letters, and null/empty string
+
+  @Test (timeout = 5000, expected = IllegalArgumentException.class)
+  public void testIsNounNull() {
+    assertFalse(wordNet6TwoAncestors.isNoun(null));
+  }
+
+  @Test
+  public void testIsNounFalse2() {
+    assertFalse(wordNet6TwoAncestors.isNoun(""));
+  }
+
+  @Test
+  public void testIsNounFalse3() {
+    assertFalse(wordNet6TwoAncestors.isNoun("aa"));
+  }
 
   @Test
   public void testDistance() {
     assertEquals(3, wordNet6TwoAncestors.distance("e", "b"));
   }
-  
-  //TODO write 2 testDistance test cases for hypernyms11AmbigousAncesor
-  //TODO write 2 testDistance test cases for hypernymsManyPathsOneAncestor
+
+  @Test
+  public void testDistance1() {
+    wordNet6TwoAncestors = new WordNet("wordnet-test-files/synsets11.txt", "wordnet-test-files/hypernyms11AmbiguousAncestor.txt");
+    assertEquals(2, wordNet6TwoAncestors.distance("j", "a"));
+  }
+
+  @Test
+  public void testDistance2() {
+    wordNet6TwoAncestors = new WordNet("wordnet-test-files/synsets11.txt", "wordnet-test-files/hypernyms11AmbiguousAncestor.txt");
+    assertEquals(2, wordNet6TwoAncestors.distance("e", "b"));
+  }
+
+  @Test
+  public void testDistance3() {
+    wordNet6TwoAncestors = new WordNet("wordnet-test-files/synsets11.txt", "wordnet-test-files/hypernyms11ManyPathsOneAncestor.txt");
+    assertEquals(1, wordNet6TwoAncestors.distance("h", "f"));
+  }
+
+  @Test
+  public void testDistance4() {
+    wordNet6TwoAncestors = new WordNet("wordnet-test-files/synsets11.txt", "wordnet-test-files/hypernyms11ManyPathsOneAncestor.txt");
+    assertEquals(3, wordNet6TwoAncestors.distance("a", "g"));
+  }
+
 
   @Test
   public void testSap() {
     assertEquals("a", wordNet6TwoAncestors.sap("e", "b"));
   }
-  
-  //TODO write 2 testSap test cases for hypernyms11AmbigousAncesor
-  //TODO write 2 testSap test cases for hypernymsManyPathsOneAncestor
+
+  @Test
+  public void testSap1() {
+    wordNet6TwoAncestors = new WordNet("wordnet-test-files/synsets11.txt", "wordnet-test-files/hypernyms11AmbiguousAncestor.txt");
+    assertEquals("j", wordNet6TwoAncestors.sap("j", "a"));
+  }
+
+  @Test
+  public void testSap2() {
+    wordNet6TwoAncestors = new WordNet("wordnet-test-files/synsets11.txt", "wordnet-test-files/hypernyms11AmbiguousAncestor.txt");
+    assertEquals("c", wordNet6TwoAncestors.sap("e", "h"));
+  }
+
+  @Test
+  public void testSap3() {
+    wordNet6TwoAncestors = new WordNet("wordnet-test-files/synsets11.txt", "wordnet-test-files/hypernyms11ManyPathsOneAncestor.txt");
+    assertEquals("f", wordNet6TwoAncestors.sap("j", "a"));
+  }
+
+  @Test
+  public void testSap4() {
+    wordNet6TwoAncestors = new WordNet("wordnet-test-files/synsets11.txt", "wordnet-test-files/hypernyms11ManyPathsOneAncestor.txt");
+    assertEquals("f", wordNet6TwoAncestors.sap("a", "h"));
+  }
 
 }
