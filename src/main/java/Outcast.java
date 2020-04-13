907caf0c61f2dd0f50ac7dee4797973df0a23d05
Jordan Muniz <munizj21@trinityprep.org>
2020-04-13 15:28:14 -0400

    chore: Fix Checkstyle warnings

--- src/main/java/Outcast.java
+++ src/main/java/Outcast.java
@@ -1,49 +1,52 @@
+
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
+/**
+ * Outcast class.
+ */
 public class Outcast {
-  private WordNet wordNet;
-  //constructor takes a WordNet object
-  public Outcast(WordNet wordnet) {
-    wordNet = wordnet;
-  }
+    /**
+     * Wordnet object for outcast check.
+     */
+    private WordNet wordNet;
 
-  // given an array of WordNet nouns, return an outcast
-  public String outcast(String[] nouns) {
-    int maxdist = -1;
-    String ret = "";
-    for (int i = 0; i<nouns.length;i++)
-    {
-      int curmax = 0;
+    public Outcast(WordNet wordnet) {
+        wordNet = wordnet;
+    }
 
-      // Fix illegal arg exceptions
+    // given an array of WordNet nouns, return an outcast
+    public String outcast(String[] nouns) {
+        int maxdist = -1;
+        String ret = "";
+        for (int i = 0; i < nouns.length; i++) {
+            int curmax = 0;
 
-      for (int j = 0; j<nouns.length;j++)
-      {
-        if(nouns[i]!=nouns[j])
-        {
-          curmax += wordNet.distance(nouns[i],nouns[j]);
+            // Fix illegal arg exceptions
+
+            for (int j = 0; j < nouns.length; j++) {
+                if (nouns[i] != nouns[j]) {
+                    curmax += wordNet.distance(nouns[i], nouns[j]);
+                }
+            }
+            if (curmax > maxdist) {
+                maxdist = curmax;
+                ret = nouns[i];
+            }
+
         }
-      }
-      if(curmax>maxdist)
-      {
-        maxdist = curmax;
-        ret = nouns[i];
-      }
 
+        return ret;
     }
 
-    return ret;
-  }
 
-  // see test client below
-  public static void main(String[] args) {
-    WordNet wordnet = new WordNet(args[0], args[1]);
-    Outcast outcast = new Outcast(wordnet);
-    for (int t = 2; t < args.length; t++) {
-      In in = new In(args[t]);
-      String[] nouns = in.readAllStrings();
-      StdOut.println(args[t] + ": " + outcast.outcast(nouns));
+    public static void main(String[] args) {
+        WordNet wordnet = new WordNet(args[0], args[1]);
+        Outcast outcast = new Outcast(wordnet);
+        for (int t = 2; t < args.length; t++) {
+            In in = new In(args[t]);
+            String[] nouns = in.readAllStrings();
+            StdOut.println(args[t] + ": " + outcast.outcast(nouns));
+        }
     }
-  }
 }
--- src/main/java/WordNet.java
+++ src/main/java/WordNet.java
@@ -1,41 +1,50 @@
+
 import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
+
 import java.util.HashMap;
 import java.util.HashSet;
 
-
+/**
+ * Wordnet class.
+ */
 public class WordNet {
-    private HashMap<String, Bag<Integer>> nouns; // maps noun to Bag with ids
-    private HashMap<Integer, String> idmap; //maps id to synset
+    /**
+     * Maps noun to bag with ids.
+     */
+    private HashMap<String, Bag<Integer>> nouns;
+    /**
+     * Maps id to sysnet.
+     */
+    private HashMap<Integer, String> idmap;
+    /**
+     * Digraph of Wordnet.
+     */
     private Digraph graph;
+    /**
+     * SAP of Wordnet.
+     */
     private SAP sap;
+    /**
+     * HashSet for quick lookup of nouns.
+     */
     private HashSet<String> strnouns;
 
-    // constructor takes the name of the two input files
-    // going to need to find a way to associate an id with its synset
     public WordNet(String synsets, String hypernyms) {
         nouns = new HashMap<>();
         idmap = new HashMap<>();
         In reader = new In(synsets);
-        int V = 0;
+        int v = 0;
         while (reader.hasNextLine()) {
-            V++;
-
-            //split the line
-
+            v++;
             String[] line = reader.readLine().split(",");
             int id = Integer.parseInt(line[0]);
             String synset = line[1];
-
-            // grab the id
-            // grab the synset out of the current line
             String[] ns = synset.split(" ");
 
             for (int i = 0; i < ns.length; i++) {
                 Bag<Integer> bag;
-
-
                 if (nouns.containsKey(ns[i])) {
                     bag = nouns.get(ns[i]);
                 } else {
@@ -45,71 +54,52 @@ public class WordNet {
                 nouns.put(ns[i], bag);
             }
             idmap.put(id, synset);
-
-            // for each noun in the synset
-            // if that noun is not in your HashMap
-            // create an entry with the id
-            // else
-            // add the current id to the Bag associated with that noun
         }
 
-        // use the reader to read the hypernyms file
         reader = new In(hypernyms);
 
-        graph = new Digraph(V); // each id is a vertex
+        graph = new Digraph(v); // each id is a vertex
         while (reader.hasNextLine()) {
             String[] line = reader.readLine().split(",");
             int id = Integer.parseInt(line[0]);
             for (int i = 1; i < line.length; i++) {
                 graph.addEdge(id, Integer.parseInt(line[i]));
             }
-
-            // grab the current line,
-            // add an edge to graph for each pair of hypernyms (remember that id of the main sysnet if first)
-            // (all hypernyms come after)
         }
-        // creat a SAP object with your Digraph
-        if(!rootedDi()) throw new IllegalArgumentException();
+        if (!rootedDi()) throw new IllegalArgumentException();
         sap = new SAP(graph);
         strnouns = new HashSet<>(nouns.keySet());
     }
-    private boolean rootedDi()
-    {
-       int roots = 0;
-       for (int i = 0; i<graph.V();i++)
-       {
-           if(!graph.adj(i).iterator().hasNext())
-           {
-               roots++;
-               if(roots>1) return false;
-           }
-       }
-       return roots==1;
-    }
-    // returns all WordNet nouns
-    public Iterable<String> nouns() {
-       return strnouns;
+
+    private boolean rootedDi() {
+        int roots = 0;
+        for (int i = 0; i < graph.V(); i++) {
+            if (!graph.adj(i).iterator().hasNext()) {
+                roots++;
+                if (roots > 1) return false;
+            }
+        }
+        return roots == 1;
     }
 
-    // is the word a WordNet noun?
+    // returns all WordNet nouns
+    public Iterable<String> nouns() {
+        return strnouns;
+    }
+
     public boolean isNoun(String word) {
-        if(word==null) throw new IllegalArgumentException();
+        if (word == null) throw new IllegalArgumentException();
         return strnouns.contains(word);
     }
 
-    // distance between nounA and nounB (defined below)
     public int distance(String nounA, String nounB) {
-        return sap.length(nouns.get(nounA),nouns.get(nounB));
+        return sap.length(nouns.get(nounA), nouns.get(nounB));
     }
 
-    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
-    // in a shortest ancestral path (defined below)
-    // return sysnet with id
     public String sap(String nounA, String nounB) {
-        return idmap.get(sap.ancestor(nouns.get(nounA),nouns.get(nounB)));
+        return idmap.get(sap.ancestor(nouns.get(nounA), nouns.get(nounB)));
     }
 
-    // do unit testing of this class
     public static void main(String[] args) {
 
     }
